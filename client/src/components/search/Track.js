62e591ec79f69ca236999e3380fd59e00ecb92f5
Fabio <famarcellus12@gmail.com>
2021-03-20 10:37:41 -0400

    Bug fix: Music previews play correct tracks after searches

--- client/src/components/search/Track.js
+++ client/src/components/search/Track.js
@@ -1,7 +1,16 @@
 import AddIcon from "../../assets/add.svg";
 import ExplicitIcon from "../../assets/explicit.svg";
+import React, { useRef } from "react";
 
 const Track = ({ musicResult }) => {
+
+    const audioRef = useRef();
+
+    if(audioRef.current) {
+        audioRef.current.load();
+    }
+
+    
     return (
       <div className="card result">
           <div className="card-body">
@@ -10,7 +19,7 @@ const Track = ({ musicResult }) => {
               <h5 className="card-title header">{musicResult.artist.name} - {musicResult.title}</h5>
               <img className="add-icon" src={AddIcon} height="50px" width="50px" alt="add-icon"/>
               {musicResult.explicit_content_lyrics && <img className="explicit-icon" src={ExplicitIcon} alt="explicit icon" width="90px" height="55" />}
-              <audio className="audio" controls>
+              <audio className="audio" controls ref={audioRef}>
                   <source src={musicResult.preview} type="audio/mpeg"/>
                   Your browser does not support the audio tag.
               </audio>
