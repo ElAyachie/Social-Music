2a3768e8f89b62c40982600910aca751d2da007b
Fabio <famarcellus12@gmail.com>
2021-03-20 08:59:58 -0400

    Added Explicit Content Warning to tracks

--- client/src/components/search/Track.js
+++ client/src/components/search/Track.js
@@ -1,4 +1,5 @@
 import AddIcon from "../../assets/add.svg";
+import ExplicitIcon from "../../assets/explicit.svg";
 
 const Track = ({ musicResult }) => {
     return (
@@ -8,6 +9,7 @@ const Track = ({ musicResult }) => {
               <br />
               <h5 className="card-title header">{musicResult.artist.name} - {musicResult.title}</h5>
               <img className="add-icon" src={AddIcon} height="50px" width="50px" alt="add-icon"/>
+              {musicResult.explicit_content_lyrics && <img className="explicit-icon" src={ExplicitIcon} alt="explicit icon" width="90px" height="55" />}
               <audio className="audio" controls>
                   <source src={musicResult.preview} type="audio/mpeg"/>
                   Your browser does not support the audio tag.
