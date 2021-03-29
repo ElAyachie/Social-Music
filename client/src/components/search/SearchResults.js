503123ad86de96cd54556d0623ae1223f1e4569b
Fabio <famarcellus12@gmail.com>
2021-03-29 17:45:26 -0400

    Imported CSSTransitions

--- client/src/components/search/SearchResults.js
+++ client/src/components/search/SearchResults.js
@@ -1,6 +1,16 @@
 import Track from './Track'
+import React, { useState } from "react"
+import { CSSTransition } from "react-transition-group"
 
 const SearchResults = ({ musicResults }) => {
+  // const [showTracks, setShowTracks] = useState(false);
+
+  function loadTracks() {
+    setShowTracks(!showTracks);
+  }
+
+
+
   return (
     <div className="results">
       {musicResults.map((musicResult) => (
