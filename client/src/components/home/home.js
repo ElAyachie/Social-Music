548d28d214a5c98f638f012ef18feff886d23ae6
Fabio <famarcellus12@gmail.com>
2021-04-01 20:13:37 -0400

    Implemented Artist and Tracks section for Profile

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "./home.scss";
+import TopMusic from "../profiles/TopMusic"
 
 
 export default class Home extends Component {
--- client/src/components/profiles/TopMusic.js
+++ client/src/components/profiles/TopMusic.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import "./profiles.scss";
+import Artists from "./Artists";
+import UserTracks from "./UserTracks"
+import { Tabs, Tab } from 'react-bootstrap';
+
 
 
 
 function TopMusic() {
 
+
     return (
-        <div>
-        
+        <div className="music-section">
+            <Tabs defaultActiveKey="Artists" id="uncontrolled-tab-example">
+                <Tab eventKey="Artists" title="Artists">
+                    <div className="list">
+                        <Artists />
+                        <Artists />
+                        <Artists />
+                        <Artists />
+                        <Artists />
+                    </div>         
+                </Tab>
+                <Tab eventKey="Tracks" title="Tracks">
+                    <UserTracks />
+                    <UserTracks />
+                    <UserTracks />
+                    <UserTracks />
+                </Tab>
+                <Tab eventKey="Albums" title="Albums">
+                    {/* <Albums /> */}
+                </Tab>
+            </Tabs>
         </div>
     )
 }
